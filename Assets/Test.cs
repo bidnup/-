44ab7e8062d9c3c456f0169d7d08a2618cda5dc1
harukazu <harumasa456@gmail.com>
2019-10-29 23:53:28 +0900

    forと配列の課題

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+
+
+
+    private void Start()
+    {
+        int array;
+        {
+            int[] points = { 19, 48, 78, 47, 97 };
+            for (int num = 0; num < 4; num++) { Debug.Log(points[num]); };
+            for (int i = 4; i > 0; i--) { Debug.Log(points[i]); }
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
+
+
+
